954d8d9dddc4d5cfd9a908d9223b813fd66d1640
servady <servadyur@gmail.com>
2022-07-21 21:44:24 +0300

    Added server app's configuration

--- internal/pkg/config/config.go
+++ internal/pkg/config/config.go
@@ -1,1 +1,8 @@
 package config
+
+// ServerConfig holds server application's configuration.
+type ServerConfig struct {
+	Address        string `yaml:"address"`
+	Port           int    `yaml:"port"`
+	DataSourceName string `yaml:"dsn"`
+}
