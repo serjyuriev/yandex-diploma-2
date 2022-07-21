191b01c98c35a4521a59bbd6035da8d42ceda18d
servady <servadyur@gmail.com>
2022-07-21 21:19:51 +0300

    Added model for login-password item

--- internal/pkg/models/models.go
+++ internal/pkg/models/models.go
@@ -1,1 +1,10 @@
 package models
+
+// LoginPasswordItem holds information about
+// single login-password entry.
+type LoginPasswordItem struct {
+	Login    string
+	Password string
+	Meta     map[string]string
+}
+
