245c395dba9451bfafa08696a966734190a83d9b
servady <servadyur@gmail.com>
2022-07-30 13:47:42 +0300

    Implemented server starting func

--- internal/app/gokeepersrv/gokeepersrv.go
+++ internal/app/gokeepersrv/gokeepersrv.go
@@ -1,15 +1,18 @@
 package gokeepersrv
 
 import (
-	"errors"
+	"fmt"
+	"net"
 	"os"
+	"time"
 
 	"github.com/rs/zerolog"
 	"github.com/serjyuriev/yandex-diploma-2/internal/app/handlers"
 	"github.com/serjyuriev/yandex-diploma-2/internal/pkg/config"
+	g "github.com/serjyuriev/yandex-diploma-2/proto"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/keepalive"
 )
-
-var errNotImplemented = errors.New("not implemented yet")
 
 // Server holds app's server-side related objects.
 type Server struct {
@@ -51,5 +54,28 @@ func NewServer() (*Server, error) {
 
 // Start launches app's server.
 func (s *Server) Start() error {
-	return errNotImplemented
+	listen, err := net.Listen("tcp", fmt.Sprintf("%s:%d", s.cfg.Address, s.cfg.Port))
+	if err != nil {
+		s.logger.
+			Err(err).
+			Caller().
+			Msgf("unable to listen on %s:%d", s.cfg.Address, s.cfg.Port)
+		return err
+	}
+	srv := grpc.NewServer(grpc.KeepaliveParams(
+		keepalive.ServerParameters{
+			MaxConnectionIdle: 5 * time.Minute,
+		},
+	))
+	g.RegisterGokeeperServer(srv, s.rpc)
+
+	s.logger.Info().Msgf("go-keeper server listening on tcp %s:%d", s.cfg.Address, s.cfg.Port)
+	if err := srv.Serve(listen); err != nil {
+		s.logger.
+			Err(err).
+			Caller().
+			Msg("unexpected error occured")
+		return err
+	}
+	return nil
 }
