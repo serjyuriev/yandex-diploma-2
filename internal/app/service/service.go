8f1e8f8d27dca44c13ccde4286e14838192e0804
servady <servadyur@gmail.com>
2022-07-30 17:34:15 +0300

    Added return value to signing up method

--- internal/app/service/service.go
+++ internal/app/service/service.go
@@ -5,6 +5,7 @@ import (
 	"crypto/sha256"
 	"fmt"
 
+	"github.com/google/uuid"
 	"github.com/rs/zerolog"
 	"github.com/serjyuriev/yandex-diploma-2/internal/app/repository"
 	"github.com/serjyuriev/yandex-diploma-2/internal/pkg/config"
@@ -31,25 +32,26 @@ func NewService(logger zerolog.Logger, repo *repository.Repository) (*Service, e
 	}, nil
 }
 
-// SignUpUser hashes user password and adds user to the database.
-func (s *Service) SignUpUser(ctx context.Context, user *models.User) error {
-	s.logger.Debug().Str("user", user.Login).Msg("signing new user up")
+// SignUpUser hashes user password and adds user to the database,
+// returning user's uuid.
+func (s *Service) SignUpUser(ctx context.Context, user *models.User) (string, error) {
+	s.logger.Debug().Str("user", user.Login).Msg("generating user's uuid")
+	user.ID = uuid.New()
 
 	s.logger.Debug().Str("user", user.Login).Msg("hashing user's password")
 	user.Password = s.hashUserPassword(user.Password)
 
-	s.logger.Debug().Str("user", user.Login).Msg("passing user info to data layer")
+	s.logger.Debug().Str("user", user.Login).Msg("passing user's info to data layer")
 	if err := s.repo.CreateUser(ctx, user); err != nil {
 		s.logger.
 			Err(err).
 			Caller().
 			Str("user", user.Login).
 			Msg("unable to create user entry in database")
-		return err
+		return "", err
 	}
 
-	s.logger.Info().Str("user", user.Login).Msg("new user was successfully signed up")
-	return nil
+	return user.ID.String(), nil
 }
 
 // hashUserPassword returns hashed with sha256 algorythm password.
