1fa6290665053a2190a3a9cf4b16afd51d30a4f6
servady <servadyur@gmail.com>
2022-07-30 17:48:59 +0300

    Implemented method for creating user entry in mongodb

--- internal/app/repository/repository.go
+++ internal/app/repository/repository.go
@@ -2,16 +2,14 @@ package repository
 
 import (
 	"context"
-	"errors"
 
 	"github.com/rs/zerolog"
 	"github.com/serjyuriev/yandex-diploma-2/internal/pkg/config"
 	"github.com/serjyuriev/yandex-diploma-2/internal/pkg/models"
+	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
 )
-
-var errNotImplemented = errors.New("not implemented yet")
 
 // Repository holds objects for data layer implementation.
 type Repository struct {
@@ -26,7 +24,7 @@ func NewRepository(logger zerolog.Logger) (*Repository, error) {
 	cfg := config.GetServerConfig()
 
 	logger.Debug().Str("module", "repo").Msg("initializing database connection")
-	client, err := mongo.Connect(context.TODO(), options.Client().ApplyURI(cfg.DataSourceName))
+	client, err := mongo.Connect(context.TODO(), options.Client().ApplyURI(cfg.Database.Address))
 	if err != nil {
 		logger.
 			Err(err).
@@ -45,5 +43,34 @@ func NewRepository(logger zerolog.Logger) (*Repository, error) {
 
 // CreateUser adds new user entry to the database.
 func (r *Repository) CreateUser(ctx context.Context, user *models.User) error {
-	return errNotImplemented
+	r.logger.Debug().Str("user", user.Login).Msg("getting users collection")
+	collection := r.client.Database(r.cfg.Database.Name).Collection("users")
+
+	r.logger.Debug().Str("user", user.Login).Msg("marshalling user's info to bson")
+	doc, err := bson.Marshal(user)
+	if err != nil {
+		r.logger.
+			Err(err).
+			Caller().
+			Str("user", user.Login).
+			Msg("unable to marshal user info to bson")
+		return err
+	}
+
+	r.logger.Debug().Str("user", user.Login).Msg("inserting new user to the database")
+	result, err := collection.InsertOne(ctx, doc)
+	if err != nil {
+		r.logger.
+			Err(err).
+			Caller().
+			Str("user", user.Login).
+			Msg("unable to insert new user to the database")
+		return err
+	}
+
+	r.logger.Debug().Str("user", user.Login).Msgf(
+		"new user was inserted to the database with ObjectID = %v",
+		result.InsertedID,
+	)
+	return nil
 }
