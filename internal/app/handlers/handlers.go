a1e63ad6314f0e495a454daee3f320f784cb6bf7
servady <servadyur@gmail.com>
2022-08-02 20:32:39 +0300

    Added handlers for all item types

--- internal/app/handlers/handlers.go
+++ internal/app/handlers/handlers.go
@@ -117,7 +117,7 @@ func (r *RPC) LoginUser(ctx context.Context, in *g.LoginUserRequest) (*g.LoginUs
 	return res, nil
 }
 
-// AddLoginItem adds new login entry in the user vault.
+// AddLoginItem adds new login entry in the user's vault.
 func (r *RPC) AddLoginItem(ctx context.Context, in *g.AddLoginItemRequest) (*g.AddLoginItemResponse, error) {
 	r.logger.Info().Str("user", in.UserID).Msg("received new login item")
 	login := &models.LoginPasswordItem{
@@ -151,6 +151,120 @@ func (r *RPC) AddLoginItem(ctx context.Context, in *g.AddLoginItemRequest) (*g.A
 	}
 
 	r.logger.Info().Str("user", in.UserID).Msg("login item was successfully added")
+	res.Error = ""
+	return res, nil
+}
+
+// AddBankCardItem adds new bank card entry in the user's vault.
+func (r *RPC) AddBankCardItem(ctx context.Context, in *g.AddBankCardItemRequest) (*g.AddBankCardItemResponse, error) {
+	r.logger.Info().Str("user", in.UserID).Msg("received new bank card item")
+	card := &models.BankCardItem{
+		Number:           in.Item.Number,
+		Holder:           in.Item.Holder,
+		Expires:          in.Item.Expires,
+		CardSecurityCode: int(in.Item.CardSecurityCode),
+		Meta:             in.Item.Meta,
+	}
+	res := new(g.AddBankCardItemResponse)
+
+	r.logger.Debug().Str("user", in.UserID).Msg("parsing user uuid")
+	userID, err := uuid.Parse(in.UserID)
+	if err != nil {
+		r.logger.
+			Err(err).
+			Caller().
+			Str("user", in.UserID).
+			Msg("unable to parse user uuid")
+		res.Error = err.Error()
+		return res, err
+	}
+
+	r.logger.Debug().Str("user", in.UserID).Msg("passing new bank card item to data layer")
+	if err := r.repo.CreateItem(ctx, card, "cards", userID); err != nil {
+		r.logger.
+			Err(err).
+			Caller().
+			Str("user", in.UserID).
+			Msg("unable to create new bank card item")
+		res.Error = err.Error()
+		return res, err
+	}
+
+	r.logger.Info().Str("user", in.UserID).Msg("bank card item was successfully added")
+	res.Error = ""
+	return res, nil
+}
+
+// AddTextItem adds new text entry in the user's vault.
+func (r *RPC) AddTextItem(ctx context.Context, in *g.AddTextItemRequest) (*g.AddTextItemResponse, error) {
+	r.logger.Info().Str("user", in.UserID).Msg("received new text item")
+	text := &models.TextItem{
+		Value: in.Item.Value,
+		Meta:  in.Item.Meta,
+	}
+	res := new(g.AddTextItemResponse)
+
+	r.logger.Debug().Str("user", in.UserID).Msg("parsing user uuid")
+	userID, err := uuid.Parse(in.UserID)
+	if err != nil {
+		r.logger.
+			Err(err).
+			Caller().
+			Str("user", in.UserID).
+			Msg("unable to parse user uuid")
+		res.Error = err.Error()
+		return res, err
+	}
+
+	r.logger.Debug().Str("user", in.UserID).Msg("passing new text item to data layer")
+	if err := r.repo.CreateItem(ctx, text, "texts", userID); err != nil {
+		r.logger.
+			Err(err).
+			Caller().
+			Str("user", in.UserID).
+			Msg("unable to create new text item")
+		res.Error = err.Error()
+		return res, err
+	}
+
+	r.logger.Info().Str("user", in.UserID).Msg("text item was successfully added")
+	res.Error = ""
+	return res, nil
+}
+
+// AddBinaryItem adds new binary entry in the user's vault.
+func (r *RPC) AddBinaryItem(ctx context.Context, in *g.AddBinaryItemRequest) (*g.AddBinaryItemResponse, error) {
+	r.logger.Info().Str("user", in.UserID).Msg("received new binary item")
+	bin := &models.BinaryItem{
+		Value: in.Item.Value,
+		Meta:  in.Item.Meta,
+	}
+	res := new(g.AddBinaryItemResponse)
+
+	r.logger.Debug().Str("user", in.UserID).Msg("parsing user uuid")
+	userID, err := uuid.Parse(in.UserID)
+	if err != nil {
+		r.logger.
+			Err(err).
+			Caller().
+			Str("user", in.UserID).
+			Msg("unable to parse user uuid")
+		res.Error = err.Error()
+		return res, err
+	}
+
+	r.logger.Debug().Str("user", in.UserID).Msg("passing new binary item to data layer")
+	if err := r.repo.CreateItem(ctx, bin, "binaries", userID); err != nil {
+		r.logger.
+			Err(err).
+			Caller().
+			Str("user", in.UserID).
+			Msg("unable to create new binary item")
+		res.Error = err.Error()
+		return res, err
+	}
+
+	r.logger.Info().Str("user", in.UserID).Msg("binary item was successfully added")
 	res.Error = ""
 	return res, nil
 }
